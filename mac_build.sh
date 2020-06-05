5f729e10ba763d704d5d9785f145156d9607311b
Nihirash <anihirash@gmail.com>
2020-06-06 01:52:09 +0300

    MacOS build fix. No blur on Retina displays

---
+++ mac_build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export QT_PATH=$HOME/Qt5.12.8/5.12.8/clang_64/
+export MACOSX_DEPLOYMENT_TARGET=10.13 
+export CMAKE_PREFIX_PATH=$QT_PATH:$CMAKE_PREFIX_PATH 
+
+rm -rf build
+mkdir build
+cd build
+cmake -DDQT4BUILD=0 -D DSDL1BUILD=0 ..
+make package
